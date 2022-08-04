aaf5a2c8dfa9eaec9625711c2fddfcf6b34f9448
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-04 23:52:13 +0400

    upd

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_events"
+EXP_NAME="svt_ucf101"
 PORT='1024'
 
 cd "$PROJECT_PATH" || exit
@@ -12,7 +12,7 @@ if [ ! -d "checkpoints/$EXP_NAME" ]; then
   mkdir "checkpoints/$EXP_NAME"
 fi
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 export WANDB_MODE="run"
 export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 
@@ -21,13 +21,13 @@ python -m torch.distributed.launch \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
-  --batch_size_per_gpu 8 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
   --do_eval True \
   --loss DINOLoss \
-  --dataset KineticsEvents \
+  --dataset Kinetics \
   --opts \
   MODEL.TWO_STREAM False \
   MODEL.TWO_TOKEN False \
