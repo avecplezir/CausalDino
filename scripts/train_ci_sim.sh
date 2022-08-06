5ac2ad88db35ff55cfaff13a77c49d4669deccf1
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-06 20:08:27 +0400

    upd

--- scripts/train_ci_sim.sh
+++ scripts/train_ci_sim.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_ci_sim"
+EXP_NAME="svt_ucf101_ci_sim_check"
 PORT='1026'
 
 cd "$PROJECT_PATH" || exit
@@ -12,7 +12,7 @@ if [ ! -d "checkpoints/$EXP_NAME" ]; then
   mkdir "checkpoints/$EXP_NAME"
 fi
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=5
 export WANDB_MODE="run"
 export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 
--- scripts/train_ci_sim_temp.sh
+++ scripts/train_ci_sim_temp.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_ci_sim"
+EXP_NAME="svt_ucf101_ci_sim_temp"
 PORT='1028'
 
 cd "$PROJECT_PATH" || exit
