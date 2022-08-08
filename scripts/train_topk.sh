0e9a9b2e824d44e1b09a0dd8072e60fb6fac8a3a
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-08 14:57:05 +0400

    upd

--- scripts/train_topk.sh
+++ scripts/train_topk.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_future2"
+EXP_NAME="svt_ucf101_topk"
 PORT='1025'
 
 cd "$PROJECT_PATH" || exit
@@ -21,7 +21,7 @@ python -m torch.distributed.launch \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
-  --batch_size_per_gpu 32 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
