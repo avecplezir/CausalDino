cea3690b9103599aff9e668afad11695a27c2a4a
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-08 23:25:22 +0400

    add small and tiny deit

--- scripts/train_gpt2.sh
+++ scripts/train_gpt2.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_gpt"
+EXP_NAME="svt_ucf101_gpt2"
 PORT='1026'
 
 cd "$PROJECT_PATH" || exit
@@ -21,7 +21,7 @@ python -m torch.distributed.launch \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
-  --batch_size_per_gpu 32 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
@@ -30,11 +30,11 @@ python -m torch.distributed.launch \
   --loss GPTLoss \
   --dataset KineticsEvents \
   --local_crops_number 0 \
-  --n_global_views 4 \
+  --n_global_views 8 \
   --freeze_last_layer 1 \
   --global_crops_scale 0.14 1 \
   --predictor GPT \
-  --n_parts 9 \
+  --n_parts 16 \
   --skip_last True \
   --opts \
   MODEL.TWO_STREAM False \
