077c775de09a8ddbcc2787f684241df59b33de9b
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-11 21:59:49 +0400

    clean

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,7 +1,5 @@
 from .dino_loss import DINOLoss
-from .mi_loss import MILoss
 from .ci_loss import CILoss
-from .mi_loss_minus import MILossMinus
 from .ci_loss_temp import CILossTemp
 from .pred_loss import PredLoss
 from .future_loss import FutureLoss
@@ -10,7 +8,5 @@ from .feature_loss import FeatureLoss
 from .gpt_loss import GPTLoss
 from .gptsim_loss import GPTSimLoss
 from .gptsim2_loss import GPTSim2Loss
-from .gpt_causal_loss_old import GPTCausalLossOld
-from .gpt_causal_loss_old2 import GPTCausalLossOld2
 from .gpt_causal_loss import GPTCausalLoss
 from .gpt_sparse_loss import GPTSparseLoss
