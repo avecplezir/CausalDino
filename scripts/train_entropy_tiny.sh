248afade18e19668f59cdd0bebc814be56a8eecd
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-12 14:38:57 +0400

    add 32 based config

--- scripts/train_entropy_tiny.sh
+++ scripts/train_entropy_tiny.sh
@@ -4,7 +4,7 @@ PROJECT_PATH="$HOME/CausalDino"
 #DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
 DATA_PATH="/mnt/data/UCF101"
 EXP_NAME="svt_ucf101_entropy_tiny_nocenter"
-PORT='1026'
+PORT='1028'
 
 cd "$PROJECT_PATH" || exit
 
