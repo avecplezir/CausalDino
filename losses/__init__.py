1c8b59463af0d9304d9174776a815e7ff0effb49
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-17 22:12:26 +0400

    clean

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,9 +1,4 @@
 from .dino_loss import DINOLoss
-from .ci_loss import CILoss
-from .ci_loss_temp import CILossTemp
-from .pred_loss import PredLoss
-from .future_loss import FutureLoss
-from .ftopk_loss import FtopkLoss
 from .feature_loss import FeatureLoss
 from .gpt_loss import GPTLoss
 from .gptsim_loss import GPTSimLoss
@@ -15,5 +10,3 @@ from .feature_time_loss import FeatureTimeLoss
 from .feature_timestable_loss import FeatureTimeStableLoss
 from .feature_timeasym_loss import FeatureAsymLoss
 from .gpt_timeasym_loss import GPTAsymLoss
-from .dino_topk_loss import DINOTopkLoss
-from .dino_log_loss import DINOLogLoss
