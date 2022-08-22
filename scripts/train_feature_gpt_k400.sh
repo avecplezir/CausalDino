91b4e2180d5d3a7ca983a70271f435981e1d444d
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-23 03:23:41 +0400

    add new sccropt

--- scripts/train_feature_gpt_k400.sh
+++ scripts/train_feature_gpt_k400.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
 
 PROJECT_PATH="$HOME/CausalDino"
-#DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
-#DATA_PATH="/mnt/data/UCF101"
-#EXP_NAME="svt_ucf101_new_stats"
 DATA_PATH="/mnt/data/Kinetics/videos_train_256p_dense_cache"
-EXP_NAME="svt_k400_tiny_32"
-PORT='1024'
+EXP_NAME="svt_k400_feature_tiny_gpt_32"
+PORT='1029'
 
 cd "$PROJECT_PATH" || exit
 
@@ -14,26 +10,31 @@ if [ ! -d "checkpoints/$EXP_NAME" ]; then
   mkdir "checkpoints/$EXP_NAME"
 fi
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4,5
 export WANDB_MODE="run"
 export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 
 python -m torch.distributed.launch \
-  --nproc_per_node=1 \
+  --nproc_per_node=2 \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
-  --batch_size_per_gpu 32 \
+  --model_name get_deit_tiny_patch16_224 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
-  --model_name get_deit_tiny_patch16_224 \
   --do_eval True \
   --eval_freq 1 \
-  --n_global_views 2 \
-  --n_parts 11 \
   --use_wandb True \
-  --loss DINOLoss \
-  --dataset Kinetics \
-  --video_extension mp4
-
+  --loss FeatureLoss \
+  --dataset KineticsEvents \
+  --local_crops_number 0 \
+  --n_global_views 4 \
+  --n_parts 5 \
+  --freeze_last_layer 1 \
+  --global_crops_scale 0.14 1 \
+  --wrapper MultiCropWrapperGPT \
+  --predictor GPT \
+  --headproba HeadProba \
+  --skip_last True
--- scripts/train_timeemb.sh
+++ scripts/train_timeemb.sh
@@ -1,9 +1,10 @@
 
 PROJECT_PATH="$HOME/CausalDino"
-#DATA_PATH="$HOME/kinetics-dataset/k400/videos_train_256p_dense_cache"
-DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101_timeemb_order_tiny"
-PORT='1029'
+DATA_PATH="/mnt/data/Kinetics/videos_train_256p_dense_cache"
+#DATA_PATH="/mnt/data/UCF101"
+#EXP_NAME="svt_ucf101_timeemb_order_tiny"
+EXP_NAME="svt_k400_te_order_tiny"
+PORT='1030'
 
 cd "$PROJECT_PATH" || exit
 
@@ -11,22 +12,22 @@ if [ ! -d "checkpoints/$EXP_NAME" ]; then
   mkdir "checkpoints/$EXP_NAME"
 fi
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=1,2
 export WANDB_MODE="run"
 export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 
 python -m torch.distributed.launch \
-  --nproc_per_node=1 \
+  --nproc_per_node=2 \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
   --model_name get_deit_tiny_patch16_224 \
-  --batch_size_per_gpu 32 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
   --do_eval True \
-  --eval_freq 4 \
+  --eval_freq 1 \
   --use_wandb True \
   --loss TimeEmbLoss \
   --dataset KineticsEvents \
@@ -40,7 +41,5 @@ python -m torch.distributed.launch \
   --skip_last True \
   --random_sampling False \
   --CE_fe_c 0.5 \
-  --CE_ef_c 0.5
-
-#
-#  --n_parts 6 \
+  --CE_ef_c 0.5 \
+  --video_extension mp4
