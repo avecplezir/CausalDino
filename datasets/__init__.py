4b5eecb9052ce9b64c85aa5896e8e3db8e6ea5de
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-23 19:59:17 +0400

    new conf

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,5 +5,5 @@ from .ucf101 import UCF101, UCFReturnIndexDataset
 from .hmdb51 import HMDB51, HMDBReturnIndexDataset
 from .kinetics_events import KineticsEvents
 from .ucf101_events import UCF101Events, UCFEventsReturnIndexDataset
-from .minerl import MineRL
+# from .minerl import MineRL
 # from .ssv2 import Ssv2  # noqa
