fde2c314101944d14121c969df60b8afce0394d2
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-24 18:23:05 +0400

    upd

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-from .kinetics import Kinetics  # noqa
-from .ucf101 import UCF101, UCFReturnIndexDataset
-from .hmdb51 import HMDB51, HMDBReturnIndexDataset
-from .kinetics_events import KineticsEvents
-from .ucf101_events import UCF101Events, UCFEventsReturnIndexDataset
+# from .kinetics import Kinetics  # noqa
+# from .ucf101 import UCF101, UCFReturnIndexDataset
+# from .hmdb51 import HMDB51, HMDBReturnIndexDataset
+# from .kinetics_events import KineticsEvents
+# from .ucf101_events import UCF101Events, UCFEventsReturnIndexDataset
+
 # from .minerl import MineRL
 # from .ssv2 import Ssv2  # noqa
