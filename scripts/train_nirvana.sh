5dfc18e2550b8120989c8eb9cf54a2c3fb1cee22
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-24 17:48:11 +0400

    upd

---
+++ scripts/train_nirvana.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROJECT_PATH="$HOME/CausalDino"
+DATA_PATH="$INPUT_PATH/something-something-v2"
+EXP_NAME="svt_ucf101_nirvana"
+PORT='1024'
+
+
+if [ ! -d "checkpoints/$EXP_NAME" ]; then
+  mkdir "checkpoints/$EXP_NAME"
+fi
+
+export WANDB_MODE="run"
+export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
+
+python -m torch.distributed.launch \
+  --nproc_per_node=1 \
+  --master_port="$PORT" \
+  train_ssl.py \
+  --arch "timesformer" \
+  --batch_size_per_gpu 32 \
+  --data_path "${DATA_PATH}" \
+  --output_dir "checkpoints/$EXP_NAME" \
+  --exp_name $EXP_NAME \
+  --model_name get_vit_base_patch16_224 \
+  --do_eval True \
+  --eval_freq 2 \
+  --n_global_views 2 \
+  --n_parts 11 \
+  --use_wandb True \
+  --loss DINOLoss \
+  --dataset Kinetics \
+  --video_extension mp4
