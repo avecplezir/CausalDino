7810be77af9f2f0aef5a5c8fbe3d31cf4005f6ec
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-26 18:50:22 +0400

    upd

--- scripts/train_mi.sh
+++ scripts/train_mi.sh
@@ -2,7 +2,7 @@
 
 PROJECT_PATH="$HOME/CausalDino"
 DATA_PATH="/mnt/data/UCF101"
-EXP_NAME="svt_ucf101"
+EXP_NAME="svt_ucf101_mi"
 PORT='1024'
 
 cd "$PROJECT_PATH" || exit
@@ -20,7 +20,7 @@ python -m torch.distributed.launch \
   --master_port="$PORT" \
   train_ssl.py \
   --arch "timesformer" \
-  --batch_size_per_gpu 32 \
+  --batch_size_per_gpu 16 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
