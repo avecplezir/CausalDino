a0ecbe342dd6f7d4e0ab4a6db0765a8d4f3f4f69
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-02 15:49:12 +0400

    new scripts

---
+++ scripts/ucf101_nirvana/train_small_ucf101_20.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PROJECT_PATH="$SOURCE_CODE_PATH/CausalDino"
+DATA_PATH="$INPUT_PATH/UCF101"
+EXP_NAME="ucf101_small_20_nirvana"
+PORT='1024'
+
+cd "$PROJECT_PATH" || exit
+
+if [ ! -d "checkpoints/$EXP_NAME" ]; then
+  mkdir -p "checkpoints/$EXP_NAME"
+fi
+
+export WANDB_MODE="run"
+export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
+
+python -m torch.distributed.launch \
+  --nproc_per_node=2 \
+  --master_port="$PORT" \
+  train_ssl.py \
+  --arch "timesformer" \
+  --batch_size_per_gpu 16 \
+  --data_path "${DATA_PATH}" \
+  --val_data_dir "${DATA_PATH}" \
+  --output_dir "${SNAPSHOT_PATH}/${EXP_NAME}" \
+  --exp_name $EXP_NAME \
+  --model_name get_deit_small_patch16_224 \
+  --do_eval True \
+  --eval_freq 1 \
+  --epochs 20 \
+  --warmup_epochs 5 \
+  --weight_decay_end 0.1 \
+  --saveckp_freq 10 \
+  --use_wandb True \
+  --loss DINOLoss \
+  --dataset Kinetics \
+  --video_extension avi \
+  --pseudo_length 239789
+
