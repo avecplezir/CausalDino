2fccd3ec2fb1837bdfe55f8b2d2433d9c2d7a144
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-02 16:03:13 +0400

    new scripts

--- scripts/ucf101/train_tiny_ucf101_ord.sh
+++ scripts/ucf101/train_tiny_ucf101_ord.sh
@@ -10,7 +10,7 @@ if [ ! -d "checkpoints/$EXP_NAME" ]; then
   mkdir "checkpoints/$EXP_NAME"
 fi
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=1
 export WANDB_MODE="run"
 export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 
@@ -20,24 +20,22 @@ python -m torch.distributed.launch \
   train_ssl.py \
   --arch "timesformer" \
   --model_name get_deit_tiny_patch16_224 \
-  --batch_size_per_gpu 32 \
+  --batch_size_per_gpu 4 \
   --data_path "${DATA_PATH}" \
   --output_dir "$PROJECT_PATH/checkpoints/$EXP_NAME" \
   --exp_name $EXP_NAME \
   --do_eval True \
   --eval_freq 4 \
   --use_wandb True \
-  --loss FeatureLoss \
+  --loss NextTokenLoss \
   --dataset KineticsEvents \
   --local_crops_number 0 \
-  --n_global_views 4 \
-  --n_parts 5 \
+  --n_global_views 32 \
+  --sampling_rate 2048 \
   --freeze_last_layer 1 \
   --global_crops_scale 0.14 1 \
   --wrapper MultiCropWrapperGPT \
   --predictor GPT \
-  --predictor_model_type gpt-micro-256-half \
   --headproba HeadProba \
-  --skip_last True
-#  --lr 3e-4 \
-#  --clip_grad 1.0 
+  --skip_last True \
+  --random_sampling False
