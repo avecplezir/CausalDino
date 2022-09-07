f00379f19410a1fbba47bd3f0b60316979290f41
avecplezir <avecplezir@gmail.com>
2022-09-07 12:55:40 +0400

    upd

--- losses/__init__.py
+++ losses/__init__.py
@@ -4,4 +4,5 @@ from .next_token_loss import NextTokenLoss
 from .timeemb_loss import TimeEmbLoss
 from .dino_gumbel_loss import DINOGumbelLoss, DINOGumbel2Loss, DINOTopkLoss, DINOGumbel3Loss, DINORandomChoiceLoss
 from .feature_loc_loss import FeatureLocLoss
+from .te_pp_loss import TEPPLoss
 from .vae_loss import VAELoss
--- losses/te_pp_loss.py
+++ losses/te_pp_loss.py
@@ -14,7 +14,7 @@ class TEPPLoss(TimeEmbLoss):
         for ie in range(1, self.n_crops):  # future encoding
             future_index = indices[:, ie].unsqueeze(1)
             s_pred_future = student.module.predictor.future_embgpt(s_pred[:, :ie], future_index=future_index)
-            s_pred_future_logits = student.module.headprob(student.module(s_pred_future))
+            s_pred_future_logits = student.module.headprob(student.module.head(s_pred_future))
             s_pred_future_proba = F.softmax(s_pred_future_logits / self.student_temp, dim=-1)
             for ip in range(0, ie): #future_prediction from past
                 loss = -torch.sum(t_enc_proba[:, ie] * torch.log(s_pred_future_proba[:, ip]), dim=-1)
@@ -30,7 +30,7 @@ class TEPPLoss(TimeEmbLoss):
         for ie in range(1, self.n_crops):  # future encoding
             future_index = indices[:, ie].unsqueeze(1)
             t_pred_future = teacher.predictor.future_embgpt(t_pred[:, :ie], future_index=future_index)
-            t_pred_future_logits = teacher.headprob(t_pred_future)
+            t_pred_future_logits = teacher.headprob(teacher.head(t_pred_future))
             t_pred_future_proba = F.softmax((t_pred_future_logits - self.center) / temp, dim=-1)
             for ip in range(0, ie): #future_prediction from past
                 loss = -torch.sum(t_pred_future_proba[:, ip] * torch.log(s_enc_proba[:, ie]), dim=-1)
