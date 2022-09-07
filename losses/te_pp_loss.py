5f4cae431ef8bc4f5325a5759d1d001568358846
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-07 12:33:36 +0400

    upd

---
+++ losses/te_pp_loss.py
@@ -0,0 +1,40 @@
+__all__ = ['TEPPLoss']
+
+import torch
+import torch.nn.functional as F
+
+from .timeemb_loss import TimeEmbLoss
+
+
+class TEPPLoss(TimeEmbLoss):
+    def compute_loss_fe(self, s_pred, t_enc_proba, student, indices):
+        total_loss = 0
+        n_loss_terms = 0
+        # ip < ie
+        for ie in range(1, self.n_crops):  # future encoding
+            future_index = indices[:, ie].unsqueeze(1)
+            s_pred_future = student.module.predictor.future_embgpt(s_pred[:, :ie], future_index=future_index)
+            s_pred_future_logits = student.module.headprob(student.module(s_pred_future))
+            s_pred_future_proba = F.softmax(s_pred_future_logits / self.student_temp, dim=-1)
+            for ip in range(0, ie): #future_prediction from past
+                loss = -torch.sum(t_enc_proba[:, ie] * torch.log(s_pred_future_proba[:, ip]), dim=-1)
+                total_loss += loss.mean()
+                n_loss_terms += 1
+        total_loss /= n_loss_terms
+        return total_loss
+
+    def compute_loss_ef(self, s_enc_proba, t_pred, teacher, indices, temp):
+        total_loss = 0
+        n_loss_terms = 0
+        # ip < ie
+        for ie in range(1, self.n_crops):  # future encoding
+            future_index = indices[:, ie].unsqueeze(1)
+            t_pred_future = teacher.predictor.future_embgpt(t_pred[:, :ie], future_index=future_index)
+            t_pred_future_logits = teacher.headprob(t_pred_future)
+            t_pred_future_proba = F.softmax((t_pred_future_logits - self.center) / temp, dim=-1)
+            for ip in range(0, ie): #future_prediction from past
+                loss = -torch.sum(t_pred_future_proba[:, ip] * torch.log(s_enc_proba[:, ie]), dim=-1)
+                total_loss += loss.mean()
+                n_loss_terms += 1
+        total_loss /= n_loss_terms
+        return total_loss
