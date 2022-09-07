aff33430c673b8bdd218f6ef230b2d80d951727c
avecplezir <avecplezir@gmail.com>
2022-09-07 12:34:26 +0400

    vae upd

--- losses/__init__.py
+++ losses/__init__.py
@@ -4,3 +4,4 @@ from .next_token_loss import NextTokenLoss
 from .timeemb_loss import TimeEmbLoss
 from .dino_gumbel_loss import DINOGumbelLoss, DINOGumbel2Loss, DINOTopkLoss, DINOGumbel3Loss, DINORandomChoiceLoss
 from .feature_loc_loss import FeatureLocLoss
+from .vae_loss import VAELoss
