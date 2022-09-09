e1f12cf3f8d47832093e2b95943421da314e7e23
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-09 15:24:32 +0400

    add script

---
+++ scripts/nirvana_epic/train_small_epic_memory_nirvana.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+PROJECT_PATH="$SOURCE_CODE_PATH/CausalDino"
+VAL_DATA_PATH="$INPUT_PATH/UCF101"
+DATA_PATH="$INPUT_PATH/videos_256"
+EXP_NAME="small_epic_memory_nirvana"
+PORT='1024'
+
+cd "$PROJECT_PATH" || exit
+
+if [ ! -d "checkpoints/$EXP_NAME" ]; then
+  mkdir -p "checkpoints/$EXP_NAME"
+fi
+
+export WANDB_MODE="run"
+export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
+
+python -m torch.distributed.launch \
+  --nproc_per_node=2 \
+  --master_port="$PORT" \
+  train_ssl.py \
+  --data_path "${DATA_PATH}" \
+  --val_data_dir "${VAL_DATA_PATH}" \
+  --output_dir "${SNAPSHOT_PATH}/${EXP_NAME}" \
+  --arch "timesformer" \
+  --video_extension MP4 \
+  --dataset_level 3 \
+  --model_name get_deit_small_patch16_224 \
+  --exp_name $EXP_NAME \
+  --batch_size_per_gpu 16 \
+  --use_wandb True \
+  --do_eval True \
+  --eval_freq 1 \
+  --epochs 20 \
+  --warmup_epochs 5 \
+  --weight_decay_end 0.1 \
+  --saveckp_freq 10 \
+  --loss MemoryLoss \
+  --teacher_pred_head True \
+  --maxlen 8 \
+  --block_size 8 \
+  --CE_fe_c 1. \
+  --CE_ef_c 0.5 \
+  --CE_ee_c 0.5 \
+  --dataset EpicNFEvents \
+  --continuous True \
+  --local_crops_number 8 \
+  --n_global_views 2 \
+  --freeze_last_layer 1 \
+  --global_crops_scale 0.4 1 \
+  --weight_decay_end 0.1 \
+  --wrapper MultiCropWrapperMemory \
+  --predictor GPT \
+  --random_sampling False \
+
+
