522de9f070b0f35125b779fab20c0fd8b33147f9
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-12 11:57:30 +0400

    upd

--- scripts/nirvana_ucf101/train_tiny_loc5_ucf101.sh
+++ scripts/nirvana_ucf101/train_tiny_loc5_ucf101.sh
@@ -2,7 +2,7 @@
 
 PROJECT_PATH="$SOURCE_CODE_PATH/CausalDino"
 DATA_PATH="$INPUT_PATH/UCF101"
-EXP_NAME="ucf101_tiny_nirvana"
+EXP_NAME="ucf101_tiny_loc5_nirvana"
 PORT='1024'
 
 cd "$PROJECT_PATH" || exit
