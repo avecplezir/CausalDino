59c40e637009f32292bfd3e26f1ef1222c52b14c
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-13 18:57:35 +0400

    upd

--- scripts/nirvana_epic/train_tiny_epic_te_bnb_nirvana.sh
+++ scripts/nirvana_epic/train_tiny_epic_te_bnb_nirvana.sh
@@ -3,7 +3,7 @@
 PROJECT_PATH="$SOURCE_CODE_PATH/CausalDino"
 VAL_DATA_PATH="$INPUT_PATH/UCF101"
 DATA_PATH="$INPUT_PATH/videos_256"
-EXP_NAME="tiny_epic_te_bnb_nirvana"
+EXP_NAME="tiny_epic_te_bnboyl0505_nirvana"
 PORT='1024'
 
 cd "$PROJECT_PATH" || exit
