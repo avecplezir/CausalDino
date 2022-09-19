295be5c762fdab5af4ee268ac9ac3e8cf84880e4
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-19 11:51:26 +0400

    upd

--- scripts/epic/tiny_epic_memory_bn.sh
+++ scripts/epic/tiny_epic_memory_bn.sh
@@ -7,7 +7,7 @@ VAL_DATA_PATH="/mnt/data/UCF101"
 DATA_PATH="/mnt/data/EPIC-KITCHENS-100/videos_256"
 PORT='1027'
 
-EXP_NAME="tiny_epic_memory1_bn"
+EXP_NAME="tiny_epic_memory2_bn"
 
 cd "$PROJECT_PATH" || exit
 
@@ -33,31 +33,31 @@ python -m torch.distributed.launch \
   --arch "timesformer" \
   --model_name get_deit_tiny_patch16_224 \
   \
-  --batch_size_per_gpu 64 \
+  --batch_size_per_gpu 32 \
   --do_eval True \
-  --sampling_rate 8 \
   --eval_freq 5 \
   --use_wandb True \
   --weight_decay_end 0.1 \
-  --num_workers 20 \
+  --num_workers 24 \
   \
   --dataset EpicNFEvents \
   --continuous True \
   --loss MemoryLoss \
   --local_crops_number 0 \
-  --n_global_views 1 \
+  --n_global_views 2 \
   --random_sampling False \
-  --maxlen 4 \
-  --block_size 4 \
-  --n_parts 4 \
+  --maxlen 8 \
+  --block_size 8 \
+  --n_parts 8 \
   --global_crops_scale 0.14 1 \
   --wrapper MultiCropWrapperGeneral \
   --predictor GPT \
   --head Projector \
   --headproba HeadProbal2Norm \
   --loss_mode memory \
-  --CE_fe_c 1 \
+  --CE_fe_c 1. \
   --CE_ef_c 0. \
+  --CE_ee_c 1. \
   --use_bn_in_head True \
   --hidden_dim_in_head 2048 \
   --teacher_prediction_type head \
