06d80c81a6f9034b26f07e21ebe2a87ca4dc4167
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-26 14:20:13 +0400

    upd

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,6 +1,6 @@
 from .base.bert_loss import BertLoss, GPTLoss, TELoss
 from .base.dino_loss import DINOLoss
-from base.feature_loss import FeatureLoss, FeatureLossAllPairs, ByolLossAllPairs
+from .base.feature_loss import FeatureLoss, FeatureLossAllPairs, ByolLossAllPairs
 
 from .gpt2_loss import GPT2Loss, GPT2MemoryLoss, TE2Loss
 from .memory_loss import GPTMemoryLoss, MemoryLoss
