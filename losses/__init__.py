6894ca45f1d997239c9ed9c4f68561a552c0409a
avecplezir <avecplezir@gmail.com>
2022-09-26 12:40:00 +0400

    upd losses

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,5 +1,5 @@
 from .dino_loss import DINOLoss
 from .feature_loss import FeatureLoss, FeatureLossAllPairs, ByolLossAllPairs
 from .bert_loss import BertLoss, GPTLoss, TELoss
-from .memory_loss import GPTMemoryLoss, MemoryLoss
 from .gpt2_loss import GPT2Loss, GPT2MemoryLoss, TE2Loss
+from .memory_loss import GPTMemoryLoss, MemoryLoss
