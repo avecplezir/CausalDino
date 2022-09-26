2bc8a8f1c77308e9481a049c6ed6d500456c2d2b
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-09-26 16:39:48 +0400

    upd

--- wrappers/gpt2_wrapper.py
+++ wrappers/gpt2_wrapper.py
@@ -7,10 +7,10 @@ class MultiCropWrapperGPT2(MultiCropWrapperBase):
     def forward_student(self, x_enc, indices, **kwargs):
         s_enc_logits = self.forward_encode(x_enc[:, 1:])
         s_pred_logits = self.forward_predict(x_enc[:, :-1], indices[:, :-1])
+        print('s_enc_logits', s_enc_logits.shape)
+        print('s_pred_logits', s_pred_logits.shape)
         return s_pred_logits, s_enc_logits
 
     def forward_teacher(self, x_enc, indices, **kwargs):
-        t_enc_logits = self.forward_encode(x_enc[:, 1:])
-        t_pred_logits = self.forward_predict(x_enc[:, :-1], indices[:, :-1])
-        return t_pred_logits, t_enc_logits
+        self.forward_student(x_enc, indices)
 
